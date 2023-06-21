a0d4ddef6adcd2604f6eda1e5bdb133e978441e5
Chakrit Wichian <chakrit@prodigy9.co>
2023-06-21 22:43:31 +0700

    fx: Add a few controllers.* builder methods
    So we don't actually have to do a full implementation for simple cases
    like a single function or a single handler.

--- httpserver/controllers/controllers.go
+++ httpserver/controllers/controllers.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"fx.prodigy9.co/config"
+	"fx.prodigy9.co/httpserver/render"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -19,4 +20,35 @@ func ReadJSON(r *http.Request, obj interface{}) error {
 		return fmt.Errorf("read json: %w", err)
 	}
 	return nil
+}
+
+// Static creates a controllers.Interface that simply renders the given object as JSON on
+// an incoming HTTP GET.
+func StaticJSON(path string, obj any) Interface {
+	return Stub{
+		MountFunc: func(_ *config.Source, router chi.Router) error {
+			router.Get(path, func(resp http.ResponseWriter, r *http.Request) {
+				render.JSON(resp, r, obj)
+			})
+			return nil
+		},
+	}
+}
+
+func FromFunc(path string, f http.HandlerFunc) Interface {
+	return Stub{
+		MountFunc: func(_ *config.Source, router chi.Router) error {
+			router.HandleFunc(path, f)
+			return nil
+		},
+	}
+}
+
+func FromHandler(path string, h http.Handler) Interface {
+	return Stub{
+		MountFunc: func(_ *config.Source, router chi.Router) error {
+			router.Handle(path, h)
+			return nil
+		},
+	}
 }
