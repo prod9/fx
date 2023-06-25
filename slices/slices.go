01be1eb7cd2967ca25cfdd20e064810578a97446
Chakrit Wichian <chakrit@prodigy9.co>
2023-06-25 17:21:36 +0700

    fx: Implement slices.In and slices.Filter

--- slices/slices.go
+++ slices/slices.go
@@ -5,6 +5,25 @@ import (
 	"sort"
 )
 
+func In[T comparable](arr []T, find T) bool {
+	for _, item := range arr {
+		if item == find {
+			return true
+		}
+	}
+	return false
+}
+
+func Filter[T any](arr []T, f func(T) bool) []T {
+	var result []T
+	for _, item := range arr {
+		if f(item) {
+			result = append(result, item)
+		}
+	}
+	return result
+}
+
 func Uniq[T constraints.Ordered](arr []T) []T {
 	var result []T
 	uniq := map[T]struct{}{}
