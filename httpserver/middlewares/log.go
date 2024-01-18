ada5a287ceafd88e3b3fb460430c7a3ed68835f7
Chakrit Wichian <chakrit@prodigy9.co>
2024-01-18 21:40:43 +0700

    fx: Reimplement CopyRequestBody as DebugRequest middleware
    * Split configuration into DEBUG_REQUEST and DEBUG_RESPONSE.
    * Use a TeeReader/httpsnoop.Wrapper for direct debug to standard out
      without bufferring any data inside the context.

--- httpserver/middlewares/log.go
+++ httpserver/middlewares/log.go
@@ -1,9 +1,6 @@
 package middlewares
 
 import (
-	"bytes"
-	"context"
-	"io"
 	"log"
 	"net/http"
 
@@ -11,44 +8,23 @@ import (
 	"github.com/felixge/httpsnoop"
 )
 
-var EnableRequestLog = config.BoolDef("LOG_REQUESTS", true)
-
 func LogRequests(cfg *config.Source) func(http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(resp http.ResponseWriter, r *http.Request) {
-			if !config.Get(cfg, EnableRequestLog) {
-				h.ServeHTTP(resp, r)
-				return
-			}
-
 			var metrics *httpsnoop.Metrics
 			log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.RequestURI)
 			defer func() {
 				if metrics == nil {
 					return
 				}
-				body := r.Context().Value("reqBody")
-				if body == nil {
-					body = ""
-				}
-				log.Printf("%s %s %s %s - HTTP %d %s\n",
-					r.RemoteAddr, r.Method, r.RequestURI, body,
+
+				log.Printf("%s %s %s - HTTP %d %s\n",
+					r.RemoteAddr, r.Method, r.RequestURI,
 					metrics.Code, metrics.Duration)
 			}()
 
 			m := httpsnoop.CaptureMetrics(h, resp, r)
 			metrics = &m
-		})
-	}
-}
-
-func CopyRequestBody(cfg *config.Source) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-			bodyBytes, _ := io.ReadAll(req.Body)
-			req.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
-			ctx := context.WithValue(req.Context(), "reqBody", string(bodyBytes))
-			next.ServeHTTP(rw, req.WithContext(ctx))
 		})
 	}
 }
