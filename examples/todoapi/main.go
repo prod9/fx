e72554be77d14a5ecbbbe164d8a6cc555d00c6c3
Chakrit Wichian <chakrit@prodigy9.co>
2024-01-18 21:41:40 +0700

    fx: Adds debug middleware to example todo app

--- examples/todoapi/main.go
+++ examples/todoapi/main.go
@@ -7,12 +7,14 @@ import (
 	"fx.prodigy9.co/examples/todoapi/auth"
 	"fx.prodigy9.co/examples/todoapi/todos"
 	"fx.prodigy9.co/httpserver/controllers"
+	"fx.prodigy9.co/httpserver/middlewares"
 )
 
 func main() {
 	err := app.Build().
 		Description("Example TODO API application").
 		DefaultAPIMiddlewares().
+		Middlewares(middlewares.DebugRequest).
 		Controllers(controllers.Home{}).
 		Mount(auth.App).
 		Mount(todos.App).
