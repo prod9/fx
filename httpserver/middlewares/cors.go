1ff5f88b6dbca5a738e28baea44e45543b0b6f8b
Chakrit Wichian <chakrit@prodigy9.co>
2024-04-12 21:23:45 +0700

    fx: Allow all headers with CORS also
    Needed for sentry to send sentry-trace and baggage
    
    See: https://docs.sentry.io/platforms/javascript/guides/sveltekit/distributed-tracing/dealing-with-cors-issues/

--- httpserver/middlewares/cors.go
+++ httpserver/middlewares/cors.go
@@ -19,6 +19,7 @@ func CORSAllowAll(cfg *config.Source) func(http.Handler) http.Handler {
 		handler = cors.New(cors.Options{
 			AllowedOrigins:   strings.Split(origin, ","),
 			AllowCredentials: true, // support fetch() with {credentials: 'include'}
+			AllowedHeaders:   []string{"*"},
 			AllowedMethods: []string{
 				http.MethodHead,
 				http.MethodGet,
