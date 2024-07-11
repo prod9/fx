8cdb3700d7fd92344c5480afdf1033a0c4ae8a4a
Chakrit Wichian <chakrit@prodigy9.co>
2024-07-11 22:07:03 +0700

    fx: Makes app builder use fragments
    So we get indepent middlewares stack for each app

--- app/app.go
+++ app/app.go
@@ -5,6 +5,7 @@ import (
 
 	"fx.prodigy9.co/cmd"
 	"fx.prodigy9.co/data/migrator"
+	"fx.prodigy9.co/httpserver"
 	"fx.prodigy9.co/httpserver/controllers"
 	"fx.prodigy9.co/httpserver/middlewares"
 	"fx.prodigy9.co/worker"
@@ -29,15 +30,15 @@ func Start(app Interface) error {
 		migrator.Embed(*app.EmbeddedMigrations())
 	}
 
-	jobs, cmds, mws, ctrs := collect(app)
+	jobs, cmds, fragment := collect(app)
 	if len(jobs) > 0 {
 		cmds = append(cmds, cmd.BuildWorkerCommand(jobs...))
 	}
-	if len(ctrs) > 0 && len(mws) == 0 { // auto-default some middlewares if controllers are added
-		mws = middlewares.DefaultForAPI()
-	}
-	if len(ctrs) > 0 || len(mws) > 0 { // don't add `serve` command if there's nothing to serve
-		cmds = append(cmds, cmd.BuildServeCommand(mws, ctrs))
+	if !fragment.IsEmpty() {
+		if fragment.HasNoMiddlewares() {
+			fragment.AddMiddlewares(middlewares.DefaultForAPI()...)
+		}
+		cmds = append(cmds, cmd.BuildServeCommandFromFragments(fragment))
 	}
 
 	return cmd.
@@ -48,23 +49,23 @@ func Start(app Interface) error {
 func collect(app Interface) (
 	[]worker.Interface,
 	[]*cobra.Command,
-	[]middlewares.Interface,
-	[]controllers.Interface,
+	*httpserver.Fragment,
 ) {
 	var (
-		jobs = app.Jobs()
-		mws  = app.Middlewares()
-		ctrs = app.Controllers()
-		cmds = app.Commands()
+		jobs     = app.Jobs()
+		cmds     = app.Commands()
+		fragment = httpserver.NewFragment(
+			app.Middlewares(),
+			app.Controllers(),
+		)
 	)
 
 	for _, child := range app.Children() {
-		childJobs, childCmds, childMws, childCtrs := collect(child)
+		childJobs, childCmds, childFragment := collect(child)
 		jobs = append(jobs, childJobs...)
 		cmds = append(cmds, childCmds...)
-		mws = append(mws, childMws...)
-		ctrs = append(ctrs, childCtrs...)
+		fragment.AddChild(childFragment)
 	}
 
-	return jobs, cmds, mws, ctrs
+	return jobs, cmds, fragment
 }
