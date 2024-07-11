1219651e4ea2a75fc2864b6d4198e456abb3a731
Chakrit Wichian <chakrit@prodigy9.co>
2024-07-11 22:14:48 +0700

    fx: Fix config load order must be reversed
    Overload with outside-in loading order is actually wrong because it
    would ignores the env var in the current environment (.env > ENV)
    
    Just blindly changing it to Load is still wrong because outside envs
    then cannot be overridden from the inside. (.env > child/.env)
    
    The correct way is to use `Load` with inside-out order so priority goes
    like this:
    
      Env > child/.env.local > child/.env > .env.local > .env > default
    
    The file `correct-output.txt` and a testcase has also been added.

--- examples/envfiles/main.go
+++ examples/envfiles/main.go
@@ -12,15 +12,17 @@ var (
 	Name3 = config.StrDef("NAME3", "Default3")
 	Name4 = config.StrDef("NAME4", "Default4")
 	Name5 = config.StrDef("NAME5", "Default5")
+	Name6 = config.StrDef("NAME6", "Default6")
 )
 
 func main() {
 	src := config.Configure()
-	config.Set(src, Name1, "Hardcoded")
+	config.Set(src, Name1, "Forced")
 
 	fmt.Println(config.Get(src, Name1))
 	fmt.Println(config.Get(src, Name2))
 	fmt.Println(config.Get(src, Name3))
 	fmt.Println(config.Get(src, Name4))
 	fmt.Println(config.Get(src, Name5))
+	fmt.Println(config.Get(src, Name6))
 }
